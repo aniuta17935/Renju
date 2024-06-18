32906dbae491b3465c1ce31f177ba5a216872a97
«aniuta17935» <ann.adamchuk2002@gmail.com>
2024-06-18 23:10:54 +0300

    project done

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Renju
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter file path, please:");
+            string filePath = Console.ReadLine();
+            // /Users/ann.adamchuk/Projects/Renju/data.txt
+            List<string> lines = new List<string>();
+            try
+            {
+                lines.AddRange(File.ReadAllLines(filePath));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            int testCasesNum;
+            try
+            {
+                testCasesNum = Convert.ToInt32(lines[0]);
+                if (testCasesNum < 1 || testCasesNum > 11)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"An error occurred: Incrrect number of test cases");
+                return;
+            }
+
+            List<TestCase> testCaseList = new List<TestCase>();
+            try
+            {
+                for (int i = 0; i < testCasesNum; i++)
+                {
+                    testCaseList.Add(new TestCase(lines.GetRange(i * 19 + 1, 19)));
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"An error occurred: Incorrect number of lines in {testCasesNum} test cases");
+                return;
+            }
+
+            for (int i = 0; i < testCasesNum; i++)
+            {
+                Console.WriteLine($"Test case {i + 1}");
+                if (!testCaseList[i].CheckTest())
+                {
+                    Console.WriteLine($"Incorrect test case");
+                    continue;
+                }
+                testCaseList[i].Process();
+                Console.WriteLine(testCaseList[i].ReturnResult());
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
---
+++ TestCase.cs
@@ -0,0 +1,147 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Renju
+{
+    public class TestCase
+    {
+        List<List<string>> _cells;
+        string _result;
+        int _resultVertical;
+        int _resultHorizontal;
+
+        public string ReturnResult()
+        {
+            if (this._result == "0")
+                return _result;
+            else
+                return _result + '\n' + _resultVertical + ' ' + _resultHorizontal;
+        }
+
+
+        public TestCase(List<string> lines)
+        {
+            _cells = new List<List<string>>();
+            foreach (string line in lines)
+            {
+                List<string> newLine = new List<string>(line.Split(' '));
+                this._cells.Add(newLine);
+            }
+            this._result = "0";
+        }
+
+        public bool CheckTest()
+        {
+            bool length = false;
+            bool content = false;
+            foreach (List<string> row in _cells)
+            {
+                length = row.Capacity == 19;
+                content = row.All(x => x == "0" || x == "1" || x == "2");
+            }
+            return length && content;
+        }
+
+
+        public void Process()
+        {
+            CheckRow();
+            CheckColumn();
+            CheckRightDiagonal();
+            checkLeftDiagonal();
+        }
+
+        private bool CheckRow()
+        {
+            for (int i = 0; i < 19; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    string player = _cells[i][j];
+                    if (player != "0" && _cells[i][j + 1] == player && _cells[i][j + 2] == player && _cells[i][j + 3] == player && _cells[i][j + 4] == player)
+                    {
+                        if (j + 5 < 19 && _cells[i][j + 5] == player)
+                            continue;
+                        if (j - 1 >= 0 && _cells[i][j - 1] == player)
+                            continue;
+                        _resultVertical = i + 1;
+                        _resultHorizontal = j + 1;
+                        _result = player;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool CheckColumn()
+        {
+            for (int j = 0; j < 19; j++)
+            {
+                for (int i = 0; i < 15; i++)
+                {
+                    string player = _cells[i][j];
+                    if (player != "0" && _cells[i + 1][j] == player && _cells[i + 2][j] == player && _cells[i + 3][j] == player && _cells[i + 4][j] == player)
+                    {
+                        if (i + 5 < 19 && _cells[i + 5][j] == player)
+                            continue;
+                        if (i - 1 >= 0 && _cells[i - 1][j] == player)
+                            continue;
+                        _resultVertical = i + 1;
+                        _resultHorizontal = j + 1;
+                        _result = player;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool CheckRightDiagonal()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    string player = _cells[i][j];
+                    if (player != "0" && _cells[i + 1][j + 1] == player && _cells[i + 2][j + 2] == player && _cells[i + 3][j + 3] == player && _cells[i + 4][j + 4] == player)
+                    {
+                        if (i + 5 < 19 && j + 5 < 19 && _cells[i + 5][j + 5] == player)
+                            continue;
+                        if (i - 1 >= 0 && j - 1 >= 0 && _cells[i - 1][j - 1] == player)
+                            continue;
+                        _resultVertical = i + 1;
+                        _resultHorizontal = j + 1;
+                        _result = player;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private bool checkLeftDiagonal()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 4; j < 19; j++)
+                {
+                    string player = _cells[i][j];
+                    if (player != "0" && _cells[i + 1][j - 1] == player && _cells[i + 2][j - 2] == player && _cells[i + 3][j - 3] == player && _cells[i + 4][j - 4] == player)
+                    {
+                        if (i + 5 < 19 && j - 5 >= 0 && _cells[i + 5][j - 5] == player)
+                            continue;
+                        if (i - 1 >= 0 && j + 1 < 19 && _cells[i - 1][j + 1] == player)
+                            continue;
+                        _resultVertical = i + 1 + 4;
+                        _resultHorizontal = j + 1 - 4;
+                        _result = player;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
